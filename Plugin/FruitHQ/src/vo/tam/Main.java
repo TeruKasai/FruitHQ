b853247509be091f14b07e77100e2b4acc8e7075
TeruKasai <tailofire2@gmail.com>
2019-09-24 14:48:55 -0700

    Moved project files
    Better organization

---
+++ Plugin/FruitHQ/src/vo/tam/Main.java
@@ -0,0 +1,21 @@
+package vo.tam;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin{
+
+	
+	//Fires when plugin is enabled
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	
+	//Fires when plugin is disabled
+	@Override
+	public void onDisable() {
+		
+		
+	}
+}
