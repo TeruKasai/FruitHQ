525a8726169f37959d203a3644c85ec541674b66
TeruKasai <tailofire2@gmail.com>
2019-11-19 15:02:53 -0800

    Update Realistic_Damage.java

--- Birds and the Bees/FruitHQ/src/vo/tam/Realistic_Damage.java
+++ Birds and the Bees/FruitHQ/src/vo/tam/Realistic_Damage.java
@@ -1,5 +1,6 @@
 package vo.tam;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.CaveSpider;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -11,8 +12,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitTask;
 
 public class Realistic_Damage implements Listener {
 
@@ -20,12 +25,14 @@ public class Realistic_Damage implements Listener {
 	public static void FallDamage(EntityDamageEvent event) {
 		if(event.getCause() == DamageCause.FALL) {
 			event.getEntity();
-			double damage = event.getDamage()*2;
+			double damage = event.getDamage()*1.5;
 			event.setDamage(damage);
 			
+			if(event.getDamage() <= 10) {
 			if (event.getEntity()instanceof LivingEntity)
-			((LivingEntity)event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 30, 2));
-		}
+			((LivingEntity)event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60, 2));
+			}
+			}
 	}
 	@EventHandler
 	public static void Bleeding(EntityDamageEvent event) {
@@ -35,7 +42,7 @@ public class Realistic_Damage implements Listener {
 			double RNG = Math.random();
 			System.out.println(RNG);
 			if(RNG <= 0.1) {
-			((LivingEntity) event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 800, 0));
+			((LivingEntity) event.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 400, 0));
 			if(event.getEntity() instanceof Player) {
 				((Player) event.getEntity()).sendMessage("You're Bleeding!");
 			}
@@ -65,4 +72,25 @@ public class Realistic_Damage implements Listener {
 		}
 	}
 	
-}
+	@EventHandler
+	public void RespawnNerf(PlayerRespawnEvent event) {
+		Player player = event.getPlayer();
+		int PlayerExp = player.getLevel();
+		player.setLevel((int) (PlayerExp * 0.8));
+		
+		
+		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
+			public void run() {
+		player.setHealth(2);
+				System.out.println("Setting food level");
+		player.getFoodLevel();
+		System.out.println(player.getFoodLevel());
+		player.setFoodLevel(10);
+		System.out.println(player.getFoodLevel());
+			}
+		}, 100L);
+	}
+	
+	
+	
+	}
